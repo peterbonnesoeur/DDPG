405e7de717c4010131c043d0cf77f6123aa42e0e
peterbonnesoeur <maxime.bonn@gmail.com>
2022-06-17 14:45:04 +0200

    Added lunar lander simulation

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ The DDPG algorithm is a variant of the [Q-learning algorithm](https://en.wikiped
 Such a problem is implemented with the OpenAI Gym [LunarLander-v2](https://www.gymlibrary.ml/environments/box2d/lunar_lander/). The Lunar Lander is a 2D game where the player must land the spacecraft on the ground. The game is played on a flat surface. The player can control the spacecraft by pressing the left and right arrow keys. The player can also accelerate the spacecraft by pressing the up arrow key. The player can also rotate the spacecraft by pressing the down arrow key.
 
 In this exercise, we just want to train a DDPG agent to play the Lunar Lander game. The parameters are the same than the ones used in the DDPG paper [Dueling Deep Q-Network Algorithms for](https://arxiv.org/abs/1511.06581).
+
+
+## Current results :
+
+![Results](docs/moon_lander.gif)
--- torch_lunar_lander.py
+++ torch_lunar_lander.py
@@ -1,9 +1,16 @@
 from ddpg_torch import Agent
+
+from gym.wrappers.monitoring import video_recorder
 import gym
 import numpy as np
-#from utils import plotLearning
+
+RECORDING = False
 
 env = gym.make('LunarLanderContinuous-v2')
+
+if RECORDING:
+    vid = video_recorder.VideoRecorder(env,path="./vid.mp4")
+
 
 agent = Agent(alpha=0.00025, beta=0.00025, input_shape=[8], tau=0.01, env=env,
               batch_size = 64, layer1_size=400, layer2_size=300, n_actions=2)
@@ -14,11 +21,14 @@ np.random.seed(0)
 
 score_history = []
 
-for i in range(1000):
+for i in range(100):
     done = False
     score = 0
     obs = env.reset()
     while not done:
+        if RECORDING:
+            env.render()
+            vid.capture_frame()
         action = agent.choose_action(obs)
         new_state, reward, done, infor, = env.step(action)
         agent.remember(obs, action, reward, new_state, int(done))
@@ -34,5 +44,4 @@ for i in range(1000):
     if i % 25 == 0:
         agent.save_models()
 
-
-    filename = 'lunar-laner.png'
+env.close()
